a8f8456a3d3cc282e315e64207273c1da0d27bb1
Gustavo Santos <96900657+pilha01@users.noreply.github.com>
2025-11-12 20:02:06 -0300

    Add files via upload

---
+++ data/IGREJA_10.js
@@ -0,0 +1,1 @@
+var json_IGREJA_10 = {"type":"FeatureCollection","name":"IGREJA_10","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":null},"geometry":{"type":"Point","coordinates":[-36.709274276630993,-10.285798368148132]}}]}
